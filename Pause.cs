adab6f21bdf9c38e49e1521ccbfbc1524c67cd97
aschmidt25 <46943909+aschmidt25@users.noreply.github.com>
2020-10-26 14:16:44 -0500

    Update Pause.cs

--- Pause.cs
+++ Pause.cs
@@ -7,14 +7,11 @@ using UnityEngine.EventSystems;
 using Rewired;
 
 public class Pause : MonoBehaviour
-{   //2 buttons, resume & exit to main menu
-    //public Button pause;
-    //public Button exitToMenu;
-
+{   
     public static Pause S;
     public GameObject pauseMenu;
     public GameObject optionsMenu;
-    //private GameObject eventSystem;
+
     private GameObject resumeButton;
 
     private void Awake()
@@ -24,8 +21,6 @@ public class Pause : MonoBehaviour
 
     private void Start()
     {
-        //S = this;
-        //eventSystem = GameObject.Find("EventSystem");
         resumeButton = GameObject.Find("ResumeButton");
 
         S.pauseMenu = this.gameObject;
@@ -34,15 +29,13 @@ public class Pause : MonoBehaviour
 
     private void Update()
     {
-        //if (ReInput.players.GetPlayer(i).GetButtonDown("Submit")
+
     }
 
     //have tom run the pausegame function when a player presses start
     //change input from Default to UI in the same spot
     public void PauseGame()
     {
-        //Debug.Log("Pause - PauseGame");
-
         //turn on pause menu
         S.pauseMenu.SetActive(true);
         EventSystem.current.SetSelectedGameObject(resumeButton, null);
@@ -79,10 +72,6 @@ public class Pause : MonoBehaviour
         InputManager.isUsingUI = true;
         SceneManager.LoadScene("Main_Menu");
 
-        //need to reset playerselect
-        //rounds = 0
-        //players = 0
-        //
 
     }
 }
