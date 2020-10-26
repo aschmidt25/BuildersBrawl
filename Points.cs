72a7137de5b4a4f8dfb05ccaa050d72b0d6a7929
aschmidt25 <46943909+aschmidt25@users.noreply.github.com>
2020-10-26 14:19:10 -0500

    Update Points.cs

--- Points.cs
+++ Points.cs
@@ -39,7 +39,7 @@ public class Points : MonoBehaviour
     //if they are pushed and they die, based on time?
     public void AddPointsForKill()
     {
-        //Debug.Log("Points given for a kill");
+        
         pointsTotal += pointsForKill;
         PrintPointsTotal();
         AddPlayerKill();
@@ -47,7 +47,7 @@ public class Points : MonoBehaviour
 
     public void AddPointsForBoardPlace(int points)
     {
-        //Debug.Log("Points given for a board place");
+        
         pointsTotal += points;
         PrintPointsTotal();
         AddPlayerBuild(points);
@@ -55,10 +55,9 @@ public class Points : MonoBehaviour
 
     public void AddPointsForOtherSide()
     {
-        //Debug.Log("Points given for a win");
         pointsTotal += pointsForOtherSide;
         PrintPointsTotal();
-        //Debug.Log("Points printed");
+        
         AddPlayerWin();
     }
 
@@ -91,16 +90,12 @@ public class Points : MonoBehaviour
     public void ChangeFaceNum(int newNum)
     {
         activeFaceNum = newNum;
-        //Debug.Log("Face for " + this.gameObject.name + " is changed to " + newNum);
+        
     }
 
     public void MakeWinner()
-    {
-        //Debug.Log(winNum);
-        
+    {       
         WinUI.S.winImageNum = winNum;
-        //Debug.Log("winImageNum = " + WinUI.S.winImageNum);
-        
     }
 
     //SAVE POINTS TO PointsStorage
@@ -158,9 +153,6 @@ public class Points : MonoBehaviour
     //Save Win Points and total Wins
     void AddPlayerWin()
     {
-        //Debug.Log("STORE POINTS");
-        //Debug.Log(GameManager.S.player1.name);
-        //Debug.Log(this.gameObject.name);
         if (this.gameObject.name.Equals(GameManager.S.player1.name))
         {
             //Debug.Log("Store Player 1 points");
@@ -186,29 +178,26 @@ public class Points : MonoBehaviour
 
     void SubtractPlayerSuicide()
     {
-        //Debug.Log("STORE POINTS");
-        //Debug.Log(GameManager.S.player1.name);
-        //Debug.Log(this.gameObject.name);
         if (this.gameObject.name.Equals(GameManager.S.player1.name))
         {
             //Debug.Log("Store Player 1 points");
             PointsStorage.P.P1Points[PointsStorage.P.total] -= pointsForSuicide;
-            //PointsStorage.P.P1Points[PointsStorage.P.wins]++;
+            
         }
         if (this.gameObject.name.Equals(GameManager.S.player2.name))
         {
             PointsStorage.P.P2Points[PointsStorage.P.total] -= pointsForSuicide;
-            //PointsStorage.P.P2Points[PointsStorage.P.wins]++;
+            
         }
         if (this.gameObject.name.Equals(GameManager.S.player3.name))
         {
             PointsStorage.P.P3Points[PointsStorage.P.total] -= pointsForSuicide;
-            //PointsStorage.P.P3Points[PointsStorage.P.wins]++;
+            
         }
         if (this.gameObject.name.Equals(GameManager.S.player4.name))
         {
             PointsStorage.P.P4Points[PointsStorage.P.total] -= pointsForSuicide;
-            //PointsStorage.P.P4Points[PointsStorage.P.wins]++;
+            
         }
     }
 }
